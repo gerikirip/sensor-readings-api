72d6425e49d1d92cabde270f33b0f5755b7b3ce4
Kirjak Gergely <gerimobb@gmail.com>
2026-08-25 11:46:32 +0200

    Initial Spring Boot project

---
+++ src/main/java/com/example/sensorreadings/SensorReadingsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sensorreadings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorReadingsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorReadingsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sensorreadings/SensorReadingsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sensorreadings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorReadingsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
